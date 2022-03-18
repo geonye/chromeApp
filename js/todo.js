702ddabedd71532028fca4ef1abb67ba3a179130
geonye <rjsdp@naver.com>
2022-03-18 23:03:14 +0900

    새로고침해도 todo 리스트 유지됨. x버튼 클릭 시 todo 리스트 삭제됨.

--- js/todo.js
+++ js/todo.js
@@ -2,21 +2,29 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-const toDos = [];
+const TODOS_KEY = "todos";
+
+let toDos = [];  //업데이트가 가능하게 let으로 설정
 
 function saveToDos() {
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
+    console.log(typeof li.id);
+    //toDo는 toDos DB에 있는 요소 중 하나
+    //클릭한 li.id와 다른 toDo는 남겨둠
+    toDos = toDos.filter((toDo) => toDo.id != parseInt(li.id));
+    saveToDos();  
 }
 
 function paintToDo(newToDo) {
     const li = document.createElement("li");
+    li.id = newToDo.id;
     const span = document.createElement("span");
-    span.innerText = newToDo;  
+    span.innerText = newToDo.text;  
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
@@ -31,9 +39,22 @@ function handleToDoSubmit(e) {
     e.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";  //enter치면 빈칸으로 바뀜
-    toDos.push(newToDo);  //toDos 배열에 newToDo를 push 
-    paintToDo(newToDo);
+    const newTodoObj = {
+        text: newToDo,
+        id: Date.now(),
+    }
+    toDos.push(newTodoObj);  //toDos 배열에 newToDo를 push 
+    paintToDo(newTodoObj);
     saveToDos(); //ToDos 저장
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit); 
+
+const savedToDos = localStorage.getItem(TODOS_KEY); 
+ 
+if(savedToDos !== null) {
+    //localstorage에 있는 string을  js object로 변경
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo); 
+}

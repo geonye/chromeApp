f6af6a8e7b7d58d06874ca566ec61ff022811945
geonye <rjsdp@naver.com>
2022-03-19 21:00:35 +0900

    주석 추가

--- js/greetings.js
+++ js/greetings.js
@@ -5,16 +5,18 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
+// 이름을 로컬저장소에 저장
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
+    localStorage.setItem(USERNAME_KEY, username);  
     paintGreetings(username);
 }
 
-function paintGreetings(username){ //화면에 TEXT가 출력되게 함
-    greeting.innerText = `Hello ${username}`;
+//화면에 TEXT가 출력
+function paintGreetings(username){ 
+    greeting.innerText = `Hello ${username} :)`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
